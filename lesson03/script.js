c50735f1e66c459f389487324bb5120fa0ea8518
DeiMos32 <72670282+DeiMos32@users.noreply.github.com>
2021-05-31 22:09:30 +0200

    lesson03hard

--- lesson03/script.js
+++ lesson03/script.js
@@ -1,34 +1,36 @@
-let money = +prompt('Ваш месячный доход?', 15000),
-    income = 'freelance',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    expenses1 = prompt('Введите обязательную статью расходов?'),
-    expenses2 = prompt('Введите обязательную статью расходов?'),
-    amount1 = +prompt('Во сколько это обойдется?'),
-    amount2 = +prompt('Во сколько это обойдется?'),
-    mission = 100000,
-    period = 12,
-    budgetMonth = money - (amount1 + amount2),
-    budgetDay = Math.floor(budgetMonth / 30);
+'use strict';
+
+let lang = prompt('Введите значение языка');
+let namePerson = prompt('Введите имя');
+let weekEn = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let weekRu = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
 
 
-    console.log(typeof money, typeof inCome, typeof deposit);
-    console.log(addExpenses.length);
-    console.log('Период равен ' + period + ' месяцам');
-    console.log('Цель заработать ' + mission + ' рублей');
-    console.log(addExpenses.toLowerCase());
-    console.log(addExpenses.split(', '));
-    console.log('Бюджет на месяц ' + budgetMonth);
-    console.log( 'Цель будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев(-а)');
-    console.log('Бюджет на день ' + budgetDay);
+if (lang === 'ru') {
+    console.log(weekRu);
+} else if (lang === 'en') {
+    console.log(weekEn);
+} else {
+    console.log('Введите правильный яызык!');
+}
 
-    if (budgetDay >= 1200) {
-        console.log('У вас высокий уровень дохода');
-    } else if (budgetDay < 1200 && budgetDay >=600) {
-        console.log('У вас средний уровень дохода');
-    } else if (budgetDay < 600 && budgetDay >= 0) {
-        console.log('К сожалению у вас уровень дохода ниже среднего');
-    } else {
-        console.log('Что-то пошло не так');
-    }
+switch (lang) {
+    case 'ru':
+        console.log(weekRu);
+        break;
+    case 'en':
+        console.log(weekEn);
+        break;
+}
+
+let week = [
+     [weekEn],
+     [weekRu]
+];
+
+console.log((namePerson === 'Артем') ? 'директор' : namePerson === 'Максим' ? 'преподователь' : 'ученик');
+
+
+
+
